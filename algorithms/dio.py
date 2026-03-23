72d6b64e1170c60f9be1a506fb0e8b05ac2cc196
korguchi <korguchi@gmail.com>
2026-03-23 11:23:18 +0900

    Add threshold option to DIO estimator

--- algorithms/dio.py
+++ algorithms/dio.py
@@ -12,15 +12,20 @@ class DIOPitchAlgorithm(ThresholdPitchAlgorithm):
     ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
         audio64 = audio.astype(np.float64)
         frame_period = self.hop_size / self.sample_rate * 1000.0  # ms
+        # DIO expects a special range.
+        # Map threshold from [0,1] to [0.02,0.2]
+        norm_threshold = 0.02 + threshold * (0.2 - 0.02)
+
         f0, t = pw.dio(
             audio64,
             self.sample_rate,
             f0_floor=self.fmin,
             f0_ceil=self.fmax,
             frame_period=frame_period,
+            allowed_range=norm_threshold,
         )
         f0 = pw.stonemask(audio64, f0, t, self.sample_rate)
         return t, f0, (f0 >= self.fmin).astype(np.float32)
 
     def _get_default_threshold(self) -> float:
-        return 0.5
+        return 0.444
